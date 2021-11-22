c0d8ff0a2b5fe1902eb0be7b3ae74b1db607a0cb
Bhawanishekhawat <bhawanishekhawat.11@gamil.com>
2021-11-22 12:58:27 +0530

    basic Security

--- student_data/src/main/java/poc2/student_data/controller/StudentController.java
+++ student_data/src/main/java/poc2/student_data/controller/StudentController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,19 +24,22 @@ public class StudentController {
 	StudentService studentService;
 
 	@PostMapping("/saveStudent")
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_STUDENT')")
 	public ResponseEntity<Student> saveStudent(@RequestBody @Valid Student student) {
 		Student studentSave = studentService.saveStudent(student);
 		return ResponseEntity.status(HttpStatus.CREATED).body(studentSave);
 	}
 
 	@GetMapping("/getAll")
+	@PreAuthorize("hasAuthority('student:read')")
 	public ResponseEntity<List<Student>> getAllStudent() {
 		List<Student> getlist = studentService.getAlldata();
 		return ResponseEntity.status(HttpStatus.OK).body(getlist);
 
 	}
 
-	@GetMapping("/getbyId/{studentId}")
+	@GetMapping("/getById/{studentId}")
+	@PreAuthorize("hasAuthority('student:read')")
 	public ResponseEntity<Student> findById(@PathVariable Long studentId){
 		Student student = studentService.searchById(studentId);
 		return ResponseEntity.status(HttpStatus.OK).body(student);
